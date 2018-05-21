549bf7002df2ffc96b9330a9a74676125f1ac628
silif <1054927886@qq.com>
2018-05-21 11:04:21 +0800

    readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# blog 系统后台
+此项目是 express.js 搭建的博客后台系统，数据库是 mysql;支持文章按id查询、文章按标查询、文章上传、文章归档查询
+### 使用
+先按照配置建好数据库、再按照model建立好表、启动数据库服务
+```git 
+clone git@github.com:silif/blog-express.git
+```
+```
+cd blog-express
+```
+```
+npm run install
+```
+```
+npm run start
+```
+
+### 其他
+- [sequelizejs](https://github.com/sequelize/sequelize) 数据库操作的 orm 库，简化数据库操作
+- [multer](https://github.com/expressjs/multer) express上传 formdata 的中间件
+- [showdown](https://github.com/showdownjs/showdown) 把 markdown 转换成 html 文档的库
+- [nodemon](https://github.com/remy/nodemon) 监控 express 代码变动并自动重启服务器
+### todo
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var routes = require('./routers/index');
 const pug = require('pug');
 var logger = require('morgan');
 var cors = require('cors')
@@ -12,14 +11,9 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 app.use(cors())
 app.use(helmet())
+var routes = require('./routers/index');
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', routes);
-
-app.get('/aaa', function(req, res) {
-    res.json({
-        msg:"success"
-    })
-})
 app.use(function (req, res, next) {
 	next(createError(404));
 });
@@ -27,7 +21,6 @@ app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
 	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
